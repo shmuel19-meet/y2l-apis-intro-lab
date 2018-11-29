dffbe2a5ebd4bf80715b059452b53b6dc33d35fe
Shmuel19-meet <shmuel19-meet@meet.mit.edu>
2018-11-29 18:57:54 +0200

    Finished everything

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def movies():
                     "image_url": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2018/02/KXC1W2-920x584.jpg"
                     }
                     """
-    return render_template('movie.html', movie={})
+    parsed_obj = json.loads(json_string)
+    return render_template('movie.html', movie=parsed_obj)
 
 
 @app.route('/tvshows')
@@ -55,10 +56,12 @@ def tv_shows():
     ]
     }]    
     """
+    parsed_obj = json.loads(json_string)
+    #print(parsed_obj)
     # Write code here to take the `json_string` and return list of movies to the user
 
 
-    return render_template('tv_shows.html')
+    return render_template('tv_shows.html', lst = parsed_obj)
 
 
 ############################
@@ -74,7 +77,10 @@ def dog_breeds():
     Do a GET request to the link above to get all dog breeds and return them
     to them as a list to the user as a bullet pointed list
     """
-    return render_template('dogs.html')
+    #parsed_stuff = requests.get("https://dog.ceo/api/breeds/list/all")
+    #parsed_stuff = parsed_stuff.encode('utf8')
+    #parsed_stuff = json.loads(parsed_stuff)
+    return render_template('dogs.html', dogs = json.loads(requests.get("https://dog.ceo/api/breeds/list/all").content))
 
 if __name__ == '__main__':
     app.run(debug=True)
